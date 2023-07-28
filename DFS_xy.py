daa30cadcc9237653893416cfe193b63dddf7bf7
YOONHO <you@example.com>
2023-07-28 17:00:51 +0900

    dfs 좌표 탐색 수정_) visited가 arr과 같은 역할을 하므로 삭제 index 범위를 확인하는 if 로직 하나로 수정

--- DFS_xy.py
+++ DFS_xy.py
@@ -3,33 +3,30 @@ from input import input_xy
 # 1. 좌표 입력
 arr = input_xy.generate_arr_xy()
 print(arr)
-visited = [[0 for _ in range(len(arr[0]))] for _ in range(len(arr))]
-print(visited)
 
 
 # 2. dfs 탐색
-def dfs(arr, visited, x, y):
-    if visited[x][y] or arr[x][y] == 1:
-        return
-    visited[x][y] = 1
-    _x, _y, x_, y_ = x - 1, y - 1, x + 1, y + 1
+def dfs(x, y):
+    if x < 0 or x > len(arr) - 1 or y < 0 or y > len(arr[0]) - 1:
+        return False
+    if not arr[x][y]:
+        arr[x][y] = 1
+        _x, _y, x_, y_ = x - 1, y - 1, x + 1, y + 1
 
-    if 0 <= x < len(arr) - 1:
-        dfs(arr, visited, x_, y)
-    if 0 < x < len(arr):
-        dfs(arr, visited, _x, y)
-    if 0 < y < len(arr[0]):
-        dfs(arr, visited, x, _y)
-    if 0 <= y < len(arr[0]) - 1:
-        dfs(arr, visited, x, y_)
+        dfs(x_, y)
+        dfs(_x, y)
+        dfs(x, _y)
+        dfs(x, y_)
+        return True
+    return False
 
 
 result = 0
 for i in range(len(arr)):
     for j in range(len(arr[0])):
-        if not arr[i][j] and not visited[i][j]:
-            dfs(arr, visited, i, j)
-            result += 1
+        if not arr[i][j]:
+            if dfs(i, j):
+                result += 1
 
 # 3. 전체 그래프 개수 출력
 print(result)
