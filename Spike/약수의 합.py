a9596208da55ebe19c50bc0313747882c8930c5d
yoonho <qufdbsgh431@gmail.com>
2023-09-06 21:35:56 +0900

    약수의 합 문제 풀이 참조 구현

--- Spike/약수의 합.py
+++ Spike/약수의 합.py
@@ -1,10 +1,23 @@
 import sys
-case = int(input())
-sum_result = [0] * 1000001
-for i in (1, 1000000):
-    sum_result[i] = (i * (n // i))
-for i in range(case):
-    n = int(sys.stdin.readline())
-    sum_result = 0
 
-    print(sum_result)
+MAX = 1000000
+case = int(input())
+# DP 1로 초기화
+dp = [1] * (MAX + 1)
+
+sum_result = [0] * (MAX +1)
+
+for i in range(2, MAX + 1):
+    t = 1
+    while i * t <= MAX:
+        dp[i * t] += i
+        t += 1
+
+for i in range(1, MAX + 1):
+    sum_result[i] = sum_result[i-1] + dp[i]
+
+
+for _ in range(case):
+    n = int(sys.stdin.readline())
+
+    print(sum_result[n])
