46f298d097e748c10107a8660fb486c4fe961103
yoonho <qufdbsgh431@gmail.com>
2023-10-26 18:02:27 +0900

    다음 순열 참조 구현

---
+++ implementation/다음 순열.py
@@ -0,0 +1,26 @@
+N = int(input())
+
+N_input = list(map(int, input().split()))
+
+result = []
+
+def next(n_input: list):
+    for i in range(N - 1, 0, -1):
+        if n_input[i] > n_input[i - 1]:
+            x, y = i - 1, i
+            for j in range(N - 1, x, -1):
+                if n_input[j] > n_input[x]:
+                    n_input[j], n_input[x] = n_input[x], n_input[j]
+                    break
+            break
+
+    try:
+        result = [*n_input[:x + 1], *sorted(n_input[y:])]
+        for k in result:
+            print(k, end=' ')
+    except:
+        print(-1)
+
+
+# run
+next(N_input)
